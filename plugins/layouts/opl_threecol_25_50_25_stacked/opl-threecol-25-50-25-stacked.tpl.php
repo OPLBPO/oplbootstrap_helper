4d8d15b89fb2a993c3778e52506da054503ebede
Wendy Warren <wwarren@coldfrontlabs.ca>
2016-07-06 15:00:30 +0000

    feat(home layout): Replaced masonry cards js plugin with css - works much better and added tabs when in mobile

--- plugins/layouts/opl_threecol_25_50_25_stacked/opl-threecol-25-50-25-stacked.tpl.php
+++ plugins/layouts/opl_threecol_25_50_25_stacked/opl-threecol-25-50-25-stacked.tpl.php
@@ -7,20 +7,27 @@
 
   <?php if ($content['left'] || $content['center'] || $content['right']): ?>
     <div class="row main-page-content"> <!-- @TODO: Add extra classes -->
+      <div>
+        <ul class="home-header-tabs">
+          <li><h2 class="home-col-header-tab"><?php print t('Events');?></h2></li>
+          <li class="home-active-tab"><h2 class="home-col-header-tab"><?php print t('Collections');?></h2></li>
+          <li><h2 class="home-col-header-tab"><?php print t('Featured');?></h2></li>
+        </ul>
+      </div>
       <div class="row-height">
-        <div class="col-md-3 col-height home-events">
+        <div class="col-xs-12 col-md-3 col-height home-events home-active">
           <div class="inside">
             <h2 class="home-col-header"><?php print t('Events');?></h2>
             <?php print $content['left']; ?>
           </div>
         </div>  
-        <div class="col-md-6 col-height home-collections">
+        <div class="col-xs-12 col-md-6 col-height home-collections home-active">
           <div class="inside">
             <h2 class="home-col-header"><?php print t('Collections');?> - <span><?php print t('Books, music, movies, magazines and more!');?></span></h2>
             <?php print $content['center']; ?>
           </div>
         </div>
-        <div class="col-md-3 col-height home-featured">
+        <div class="col-xs-12 col-md-3 col-height home-featured">
           <div class="inside">
             <h2 class="home-col-header"><?php print t('Featured');?></h2>
             <?php print $content['right']; ?>
